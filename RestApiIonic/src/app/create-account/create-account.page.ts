cd65adb61bb44471c362c1b3b3a43fa73fd41bc7
JonasBCosta <48065552+JonasBCosta@users.noreply.github.com>
2019-03-01 13:42:29 -0300

    Commit2

--- RestApiIonic/src/app/create-account/create-account.page.ts
+++ RestApiIonic/src/app/create-account/create-account.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams, ToastController } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.page.html',
   styleUrls: ['./create-account.page.scss'],
 })
-export class CreateAccountPage implements OnInit {
+export class CreateAccountPage {
 
-  constructor() { }
+   model: User;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private toast: ToastController, private userService: UsersService) {
+      this.model = new User();
+      this.model.email = 'sydney@fife';
+      this.model.password = 'pistol';
+  }
 
+  createAccount() {
+    this.userService.createAccount(this.model.email, this.model.password)
+      .then((result: any) => {
+        this.toast.create({ message: 'Usuário criado com sucesso. Token: '
+        + result.token, position: 'bottom', duration: 3000}).present();
+      })
+      .catch((error: any) => {
+        this.toast.create({ message: 'Erro ao criar o usuário. Erro: '
+        + error.error, position: 'bottom', duration: 3000}).present();
+      })
+  }
   ngOnInit() {
   }
 
 }
+    export class User{
+        email: string;
+        password: string;
+    }
